e74311456433f25db94c5dd0580507733553bb84
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-27 13:46:12 -0700

    part3 done

---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from "typography"
+import fairyGateTheme from "typography-theme-fairy-gates"
+
+const typography = new Typography(fairyGateTheme)
+const { rhythm, scale } = typography
+export { rhythm, scale, typography as default }
